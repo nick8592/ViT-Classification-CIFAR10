d566fb78bdea246f1ff361229d9645d398a489bd
nick8592 <nickybai8592@gmail.com>
2024-10-22 22:33:43 +0800

    create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import torch
+import torch.utils
+import torch.utils.data
+import torchvision
+import torchvision.transforms as transforms
+#TODO load CIFAR-10 dataset
+def dataloader():
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]
+    )
+
+    batch_size = 4
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                            download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+
+#TODO Patch Embedding
+#TODO Positional Encoding
+#TODO Transformer Encoder
+#TODO Classification Head
+
+def main():
+    dataloader()
+
+if __name__ == "__main__":
+    main()
